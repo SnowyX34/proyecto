d49b6b007c9765464c2b0f9cba61e2c9f2ce0257
Ulises Flores Martínez <ulises@example.com>
2025-08-09 03:26:41 -0600

    Resubida 1.3

--- backend/src/config/connection.ts
+++ backend/src/config/connection.ts
@@ -1,11 +1,11 @@
  import { Sequelize } from 'sequelize';
 
  const sequelize = new Sequelize(
-   process.env.DB_NAME ?? 'sqldb_1riw',
-   process.env.DB_USER ?? 'useruli',
-   process.env.DB_PASSWORD ?? 'A3FKt1R5VgFeVpwI87TtmezUO1K9HgDX',
+   process.env.DB_NAME ?? 'cotizaciones_jgpp',
+   process.env.DB_USER ?? 'ulis',
+   process.env.DB_PASSWORD ?? 'FiHjAPjq6FEpYrusamf9XP31NuPuPEwE',
    {
-     host: process.env.DB_HOST ?? 'dpg-d20kog7fte5s7391c7lg-a',
+     host: process.env.DB_HOST ?? 'dpg-d2bgu5p5pdvs73cofn2g-a',
      port: parseInt(process.env.DB_PORT ?? '5432'),
      dialect: 'postgres',
      logging: false,
