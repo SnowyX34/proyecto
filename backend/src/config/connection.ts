5ec63d0a75396d91c76637b9c9adad9060971dde
Ulises Flores Martínez <ulises@example.com>
2025-08-09 08:02:00 -0600

    Actualizacion .ENV V.1.4.3

--- backend/src/config/connection.ts
+++ backend/src/config/connection.ts
@@ -5,7 +5,7 @@
    process.env.DB_USER ?? 'ulis',
    process.env.DB_PASSWORD ?? 'FiHjAPjq6FEpYrusamf9XP31NuPuPEwE',
    {
-     host: process.env.DB_HOST ?? 'dpg-d2bgu5p5pdvs73cofn2g-a',
+     host: process.env.DB_HOST ?? 'd2bgu5p5pdvs73cofn2g-a.oregon-postgres.render.com',
      port: parseInt(process.env.DB_PORT ?? '5432'),
      dialect: 'postgres',
      logging: false,
