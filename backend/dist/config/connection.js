a1a3891d45a99be14b04bdb81890ad78d4e16d25
Ulises Flores Martínez <ulises@example.com>
2025-08-09 09:09:06 -0600

    Actualizacion .ENV V.1.4.8

--- backend/dist/config/connection.js
+++ backend/dist/config/connection.js
@@ -2,8 +2,13 @@
 var _a, _b, _c, _d, _e;
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
 const sequelize = new sequelize_1.Sequelize((_a = process.env.DB_NAME) !== null && _a !== void 0 ? _a : 'cotizaciones_jgpp', (_b = process.env.DB_USER) !== null && _b !== void 0 ? _b : 'ulis', (_c = process.env.DB_PASSWORD) !== null && _c !== void 0 ? _c : 'FiHjAPjq6FEpYrusamf9XP31NuPuPEwE', {
-    host: (_d = process.env.DB_HOST) !== null && _d !== void 0 ? _d : 'dpg-d20kog7fte5s7391c7lg-a.oregon-postgres.render.com',
+    host: (_d = process.env.DB_HOST) !== null && _d !== void 0 ? _d : 'd2bgu5p5pdvs73cofn2g-a.oregon-postgres.render.com',
     port: parseInt((_e = process.env.DB_PORT) !== null && _e !== void 0 ? _e : '5432'),
     dialect: 'postgres',
     logging: false,
--- backend/src/config/connection.ts
+++ backend/src/config/connection.ts
@@ -1,11 +1,17 @@
 import { Sequelize } from 'sequelize';
 
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 const sequelize = new Sequelize(
   process.env.DB_NAME ?? 'cotizaciones_jgpp',
   process.env.DB_USER ?? 'ulis',
   process.env.DB_PASSWORD ?? 'FiHjAPjq6FEpYrusamf9XP31NuPuPEwE',
   {
-    host: process.env.DB_HOST ?? 'dpg-d20kog7fte5s7391c7lg-a.oregon-postgres.render.com',
+    host: process.env.DB_HOST ?? 'd2bgu5p5pdvs73cofn2g-a.oregon-postgres.render.com',
     port: parseInt(process.env.DB_PORT ?? '5432'),
     dialect: 'postgres',
     logging: false,
@@ -24,5 +30,6 @@ const sequelize = new Sequelize(
     }
   }
 );
+
 
 export default sequelize;
